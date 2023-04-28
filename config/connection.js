8ba198ad39efa6de84ead4c21f72fc585b974753
cmadrid48 <cmadrid48@gmail.com>
2023-04-27 20:34:20 -0700

    Fixed connection.js, but new error present:)

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost:27017/dasocial.18-nosql",
+  process.env.MONGODB_URI || "mongodb://localhost:127.0.0.1:27017/socialDB",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
